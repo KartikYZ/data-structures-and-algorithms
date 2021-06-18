d84457476ac87712c9e08d7b0e08b8bbcfaef378
KartikYZ <67892980+KartikYZ@users.noreply.github.com>
2021-06-18 12:27:54 +0530

    characterFrequencyMap

--- algorithms/HuffmanCodes.java
+++ algorithms/HuffmanCodes.java
@@ -1,5 +1,24 @@
 package algorithms;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HuffmanCodes {
+
     
+    
+    public static Map<Character, Integer> characterFrequencyMap(String s) {
+        HashMap<Character, Integer> charFreqMap = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            int freq = charFreqMap.getOrDefault(c, 0);
+            charFreqMap.put(c, freq + 1);
+        }
+        return charFreqMap;
+    }
+
+    public static void main(String[] args) {
+        String testWord = "Hello World";
+        System.out.println(characterFrequencyMap(testWord));
+    }
 }
